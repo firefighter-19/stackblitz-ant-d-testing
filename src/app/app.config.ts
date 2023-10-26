e4be231926cb3c48ae49a2ae8e8499d5daa55342
Andrey Sazonov <fromlhz03@gmail.com>
2023-10-26 13:40:34 +0300

    chore (provide locale): add locale to config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,14 @@ import { provideRouter } from "@angular/router";
 import { provideAnimations } from "@angular/platform-browser/animations";
 
 import { routes } from "./app.routes";
+import { NZ_DATE_LOCALE, NZ_I18N, ru_RU } from "ng-zorro-antd/i18n";
+import { ru } from "date-fns/locale";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    { provide: NZ_DATE_LOCALE, useValue: ru },
+    { provide: NZ_I18N, useValue: ru_RU },
+  ],
 };
